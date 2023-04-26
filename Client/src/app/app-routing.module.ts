28e72d5958384bd591bd9b28fbf4d6e9d6306251
Al Shaikhli <ahmed.al.shaikhli@accenture.com>
2023-04-26 11:06:15 +0200

    update pages

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { RiskProfileComponent } from './risk-profile/risk-profile.component';
 import { HomeAreaDetailsComponent } from './home/home-area-code/home-area-details/home-area-details.component';
 import { WorkAreaDetailsComponent } from './home/work-area-code/work-area-details/work-area-details.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomeAreaCodeComponent } from './home/home-area-code/home-area-code.component';
 
 
 
@@ -11,9 +13,12 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'riskProfile', component: RiskProfileComponent },
+  { path: 'userProfile', component: UserProfileComponent },
 /*   { path: 'home', component: HomeComponent }, */
   { path: 'home/homeDetails', component: HomeAreaDetailsComponent },
   { path: 'home/workDetails', component: WorkAreaDetailsComponent },
+  { path: 'home/homeAreCode', component: HomeAreaCodeComponent },
+  
 
   
    // Child component route with result as a parameter
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,26 +7,25 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './user-profile/login/login.component';
-import { RegisterComponent } from './user-profile/register/register.component';
+
 import { RiskProfileComponent } from './risk-profile/risk-profile.component';
 import { HomeAreaCodeComponent } from './home/home-area-code/home-area-code.component';
 import { WorkAreaCodeComponent } from './home/work-area-code/work-area-code.component';
 import { HomeAreaDetailsComponent } from './home/home-area-code/home-area-details/home-area-details.component';
 import { WorkAreaDetailsComponent } from './home/work-area-code/work-area-details/work-area-details.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    LoginComponent,
-    RegisterComponent,
     RiskProfileComponent,
     HomeAreaCodeComponent,
     WorkAreaCodeComponent,
     HomeAreaDetailsComponent,
-    WorkAreaDetailsComponent
+    WorkAreaDetailsComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
